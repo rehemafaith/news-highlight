2077fab762e10052099d855059a42ed914d36522
rehemafaith <rehemafaith01@gmail.com>
2019-04-12 10:34:27 +0300

    add folders and files to be used in the project

---
+++ app/views.py
@@ -0,0 +1,13 @@
+from flask import render_template
+from app import app 
+
+#Views 
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its date 
+    '''
+    return render_template('index.html')
+
+    
